9643f7507ab74df460601f8f95a83a2bcffc9a20
Tom Roar Furunes <tom.roar@halloverden.no>
2023-11-16 14:16:13 +0100

    ADD: initial bundle setup

---
+++ src/FagforbundetNotificationApiMailerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Fagforbundet\NotificationApiMailerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class FagforbundetNotificationApiMailerBundle extends AbstractBundle {
+
+}
