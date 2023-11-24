23012bc27059ddd1557753397efd56637f4fcf4c
Tom Roar Furunes <tom.roar@halloverden.no>
2023-11-24 11:55:12 +0100

    ADD: bundle setup

--- src/FagforbundetNotificationApiMailerBundle.php
+++ src/FagforbundetNotificationApiMailerBundle.php
@@ -2,8 +2,28 @@
 
 namespace Fagforbundet\NotificationApiMailerBundle;
 
+use Fagforbundet\NotificationApiMailerBundle\Transport\NotificationApiTransportFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
-class FagforbundetNotificationApiMailerBundle extends AbstractBundle {
+final class FagforbundetNotificationApiMailerBundle extends AbstractBundle {
+
+  public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void {
+    $alias = $this->getContainerExtension()->getAlias();
+
+    $container->services()
+      ->set($alias . '.transport_factory', NotificationApiTransportFactory::class)
+        ->args([
+          service('fagforbundet_notification_api_client.client'),
+          service('event_dispatcher'),
+          service('http_client')->ignoreOnInvalid(),
+          service('logger')->ignoreOnInvalid(),
+        ])
+        ->tag('monolog.logger', ['channel' => 'mailer'])
+        ->tag('mailer.transport_factory')
+    ;
+  }
 
 }
